18440e68a5cd0504de97ab00e00c412fa8dc4a45
EdKaval <eduardskavaleristovs@gmail.com>
2018-07-06 14:44:28 +0300

    trigonometric add

--- mgesture.lua
+++ mgesture.lua
@@ -1,17 +1,44 @@
+math = require("math")
+
 prevx, prevy = love.mouse.getPosition()
 x, y = love.mouse.getPosition()
 prevMov = false
 prevDirect = null
 
+gr = love.graphics
 
 local function getDirection(px, py, nx, ny)
-	love.graphics.print(tostring(px) .. "  " .. tostring(py) .. "  " .. tostring(nx) .. "  " .. tostring(ny), 200, 100)
+	gr.print(tostring(px) .. "  " .. tostring(py) .. "  " .. tostring(nx) .. "  " .. tostring(ny), 100, 200)
+	
+	gr.line(px, py, nx, ny)
+	gr.line(nx, py, nx, ny)
+	gr.line(px, py, nx, py)
+	
 	local xx = nx - px
 	local yy = ny - py
+	local mv = 0
+	local res = 0
 	
-	if yy == 0 then yy = 1 end
+	if (xx < 0) and (yy >= 0) then 
+		xx = -xx
+		res = math.atan(xx/yy)*57.2958 + 90 
+	elseif (xx < 0) and (yy < 0) then 
+		xx = -xx
+		yy = -yy
+		res = math.atan(yy/xx)*57.2958 + 180
+	elseif (xx >= 0) and (yy < 0) then 
+		yy = -yy
+		res = math.atan(xx/yy)*57.2958 + 270 
+	elseif (xx >= 0) and (yy >= 0) then 
+		res = math.atan(yy/xx)*57.2958 
+	end
 	
-	return xx/yy
+	gr.line(400, 400, 400+xx, 400)
+	gr.line(400+xx, 400, 400+xx, 400+yy)
+	
+	if xx == 0 then xx = 0.00001 end
+	
+	return res
 end
 
 function loadMovement()
@@ -19,8 +46,7 @@ function loadMovement()
 
 	if not love.mouse.isDown(1) then 
 		local direct = getDirection(prevx, prevy, x, y)
-		prevDirect = direct
-		love.graphics.print(tostring(prevDirect), 100, 100)
+		love.graphics.print(tostring(direct), 100, 100)
 		prevMov = false 
 		return 
 	end
@@ -31,7 +57,8 @@ function loadMovement()
 	else 
 		
 		x, y = love.mouse.getPosition()
-		
+		local direct = getDirection(prevx, prevy, x, y)
+		love.graphics.print(tostring(direct), 100, 100)
 		
 	end
 end
