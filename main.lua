97ef9acbe8c1ce29ff2e71c45aeb4291b8ae919f
Wapiozi <ggsobaka@gmail.com>
2018-07-06 11:29:49 +0300

    begin

--- main.lua
+++ main.lua
@@ -1,5 +1,114 @@
-function love.load(arg)
+local Player, Field, Enemy, Mana, AKM -- smth like place 4 drawing
 
+--[[
+	Player - is a magician
+	Field - is a background and all obstacles mot AI
+	Enemy - is an obstacle with special power and defense agil.
+	Mana - is a value of different kinds magic elements for ex Earth, Water...
+	AKM - all kind of magic, all possible magic shit	
+]]--
+
+-- Player implementation --------------------------------------------------------
+
+Player = {}
+Player.__index = Player
+Player.type = 'player'
+
+function Player:new(field, mana, x, y, vx, vy)
+	self = setmetatable({}, self)
+	self.field = field
+	self.x = x or 100
+	self.y = y or 100
+	self.angle = 0
+	self.vx = vx or 5
+	self.vy = vy or 5
+	
+	self.magic_delay = md or 1
+	self.magic_fire  = mana[fire] or 0
+	self.magic_water = mana[water] or 0
+	self.magic_air   = mana[air] or 0
+	self.magic_earth = mana[earth] or 0
+
+	return self;
+end
+
+function Player:update(dt)
+	self.magic_delay = self.magic_delay - dt
+
+	-- in making phase, mouse is used
+
+	if love.mouse.isDown(1) and self.magic_delay < 0 then
+		-- drawing process
+	end
+
+	self.x = self.x + self.vx * dt
+	self.y = self.y + self.vy * dt
+
+end
+
+function Player:draw()
+	love.graphics.draw(PlayerImg, 100, 100)
+	-- by now, it is only like that :P
+end
+
+-- Enemy Implementation --------------------------------------------
+Enemy = {}
+Enemy.__index = Enemy
+Enemy.type = 'enemy'
+
+function Enemy:new(field, hp, x, y, vx, vy, red) -- + class of enemy, warior, magician..
+	self = setmetatable({}, self)
+	self.x = x 
+	self.y = y
+	self.vx = vx
+	self.vy = vy
+	self.radius = CollisionMask or 15; 
+	self.hp = hp
+
+	 -- also, there should be some agilities of different classes
+	 -- for ex. immortal, reduce fire dmg or smth like that
+	self.fire_r  = red[f] -- if red[f] = 0 then fire cant affect
+	self.earth_r = red[e]
+	self.water_r = red[w]
+	self.air_r   = red[a]
+	return self
+end
+
+function Enemy:applyMagic(Dmg_fire, Dmg_water, Dmg_earth, Dmg_air)
+	-- check for special agil. of enemy , if no, then --> 
+	f = Dmg_fire  * self.fire_r
+	f = Dmg_water * self.water_r
+	f = Dmg_earth * self.earth_r
+	f = Dmg_air   * self.air_r
+	dmg = f + w + e + a
+	self.hp = self.hp - dmg
+
+	if self.hp < 0 then
+		-- add score
+		self.field:destroy(self)
+	end
+end
+
+local function Enemy:collision(x1, y1, r1, x2, y2, r2)
+	local distance = (x2 - x1) ^ 2 + (y2 - y1) ^ 2
+	local rdist = (r1 + r2) ^ 2
+	return distance < rdist
+end
+
+function Enemy:update(dt)
+	self.x = self.x = self.vx * dt
+	self.y = self.y = self.vy * dt
+
+	for object in pairs(self.field:getObjects()) do
+
+end
+-- Standart ------------------------------------------------------------
+
+function love.load(arg)
+	-- Sprites
+	PlayerImg = love.graphics.newImage("Wizard.jpg")
+	EnemyImg  = love.graphics.newImage("Enemy.jpg")
+	-- by now there will be only one kind of enemies
 end
 
 function love.update(dt)
