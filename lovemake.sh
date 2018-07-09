c086beddaf23090ea710c92a063bf088448f5db4
EdKaval <tankicraft263crafttanki3@gmail.com@gmail.com>
2018-07-09 14:31:48 +0300

    added magic.lua

--- lovemake.sh
+++ lovemake.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip OWS.love main.lua mgesture.lua Ball.jpg Enemy.png Wizard.jpg > /dev/null
+zip OWS.love main.lua mgesture.lua Ball.jpg Enemy.png Wizard.jpg magic.lua > /dev/null
 love OWS.love
