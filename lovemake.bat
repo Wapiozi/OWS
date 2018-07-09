44a7996ce388c45287705a4da1704f242910c981
arseni4 <arsneij18@gmail.com>
2018-07-09 03:53:25 +0300

    Added make for Windows

---
+++ lovemake.bat
@@ -0,0 +1,4 @@
+del /q game.zip
+echo CHANGE PATH TO love.exe
+powershell.exe -nologo -noprofile -command "& { Compress-Archive -Path main.lua, mgesture.lua, Ball.jpg, Enemy.png, Wizard.jpg -CompressionLevel Optimal -DestinationPath game.zip; cmd /c "copy /b C:\love-11.1.0-win64\love.exe+game.zip game.exe" ; }"
+pause
