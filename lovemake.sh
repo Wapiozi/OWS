1210bc0315d95122e4a58a4f6514cccfb577f208
EdKaval <tankicraft263crafttanki3@gmail.com>
2018-07-10 17:35:04 +0300

    Fireball.png added

--- lovemake.sh
+++ lovemake.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip OWS.love main.lua mgesture.lua Ball.jpg Enemy.png Wizard.jpg magic.lua > /dev/null
+zip OWS.love main.lua mgesture.lua Ball.jpg Enemy.png Wizard.jpg magic.lua Fireball.png > /dev/null
 love OWS.love
