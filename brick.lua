c22434b77d9620efea366a36c4ee791cde53f069
EdKaval <tankicraft263crafttanki3@gmail.com>
2018-07-11 21:29:43 +0300

    added brick class

---
+++ brick.lua
@@ -0,0 +1,26 @@
+
+Brick = {}
+Brick.__index = Brick
+
+function Brick:new(x, y, sizex, sizey, angle, image) 
+	self = setmetatable({}, self)
+	
+	x, y = pcoords(x, y)
+	sizex, sizey = pcoords(sizex, sizey)
+	
+	self.name = "brick"
+	
+	self.body = love.physics.newBody(world, x, y, "static")
+	self.body:setAngle(angle or 0)
+	self.shape = love.physics.newShape(sizex, sizey)
+	self.fixture = love.physics.newFixture(self.body, self.shape)
+	
+	self.image = image
+	self.shader = nil
+end
+
+function Brick:draw()
+	love.graphics.setColor(0.5, 0.9, 0.1)
+	love.graphics.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
+end
+
