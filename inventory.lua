36e353f359007c0e67a402eb4569f2ac479101ef
Wapiozi <ggsobaka@gmail.com>
2018-07-21 14:10:53 +0300

    dont try this at home

--- inventory.lua
+++ inventory.lua
@@ -48,8 +48,27 @@ function Inventory:loseItem(ind, player_x, player_y)
 	item1:spawn(player_x+50, player_y)
 end
 
-function Inventory:dragging()
-	--
+function Inventory:dragging(mx, my)
+	x1, y1, i1, j1= self:findWindow(mx,my)
+	if x1 ~= -1 then
+  		cursor = self.slot[i1*9+j1]
+  		local scl, w, h = imageProps(0.038, cursor.image)
+  		love.mouse.setVisible(false)
+ 		--love.mouse.setGrab(true)	
+		while love.mouse.isDown(1) do
+			love.graphics.draw(cursor.image, love.mouse.getX(), love.mouse.getY(), 0, scl)
+		end
+		mouse_x, mouse_y = love.mouse.getPosition()
+		x2, y2, i2, j2= self:findWindow(mx,my)
+		love.mouse.setVisible(true)
+ 		--love.mouse.setGrab(false)
+ 		if x2 ~= -1 then
+ 			self.slot[i1*9+j1] = self.slot[i2*9+j2]
+ 			self.slot[i2*9+j2] = cursor
+ 		else
+ 			--drop on the floor
+ 		end
+	end
 end
 	
 function Inventory:findWindow(mx, my)
@@ -59,7 +78,7 @@ function Inventory:findWindow(mx, my)
 		for j = 1, self.w do
 			if  mx >= x1 and mx <= x1 + self:fcord(30.5,"x") and
 				my >= y1 and my <= y1 + self:fcord(30,"y") then
-					return x1, y1
+					return x1, y1, i, j
 			end
 			x1 = x1 + self:fcord(30.5,"x") 
 		end 
@@ -70,12 +89,15 @@ function Inventory:findWindow(mx, my)
 end
 
 function Inventory:checkInventoryMode(mx, my)
-	x1, y1 = self:findWindow(mx,my)
+	x1, y1, i, j = self:findWindow(mx,my)
 	if x1 ~= -1 then 
 		x1 = x1 - self:fcord(0.1,"x")
 		y1 = y1 - self:fcord(0.1,"y")
 		local scl,h,w = imageProps(0.039,InvborderImg)
 		love.graphics.draw(InvborderImg, x1, y1, 0, scl)
+		if love.mouse.isDown(1) then
+			self:dragging(mx, my)
+		end
 	end
 end
 
