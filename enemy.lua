12ea04fe853e7b3822df1b7114323ce130897e4b
Vinzafy <Vinzafy>
2018-07-31 22:17:01 +0300

    Test

--- enemy.lua
+++ enemy.lua
@@ -6,12 +6,12 @@ Enemy.type = 'enemy'
 function Enemy:init()
 
 	--[[
-			there will be several parts of behaviour : 
+			there will be several parts of behaviour :
 			1) movement :
 				victim  = run from player
 				stalker	= run to player
 				neutral	= do nothing
-			2) sensor : 
+			2) sensor :
 				vision  = will start movement after eye contact
 				smell = will start movemnt after some time of being in same room with player
 				noise = will start movement if player run or doing smth loud
@@ -30,10 +30,10 @@ function Enemy:init()
 		mass = 70,
 
 		behaviour = {
-			movement_bd = "move", 
-			movement_ad = "victim", 
+			movement_bd = "move",
+			movement_ad = "victim",
 			sensor = {vision = true, smell = false, noise = true},
-			playerdist = 0 
+			playerdist = 0
 		},
 
 		timer = 5,
@@ -53,9 +53,9 @@ function Enemy:init()
 		mass = 70,
 		manaMax = 100,
 
-		behaviour = { 
-			movement_bd = "slow_move", 
-			movement_ad = "aggressive", 
+		behaviour = {
+			movement_bd = "slow_move",
+			movement_ad = "aggressive",
 			attack = "magic",
 			magic_type = {
 				q = 3, --quantity
@@ -70,7 +70,7 @@ function Enemy:init()
 		timer = 5,
 		Init = nil
 	}
-	
+
 	EnemyTypeRat.Collis = function(px, py)
 	end
 	EnemyTypeMadwizard.Collis = function(px, py)
@@ -80,18 +80,18 @@ end
 function Enemy:new(type, x, y) -- + class of enemy, warior, magician..
 
 	self = setmetatable({}, self)
-	
+
 	x, y = pcoords(x, y)
 
-	self.type = type 
+	self.type = type
 	self.image = self.type.image
 	self.imgturn = self.type.imgturn
 	self.scale, self.width, self.height = imageProps(self.type.size, self.image)
-	
+
 	self.body = love.physics.newBody(world, x, y, "dynamic")
 	self.body:setAngle(0)
 	self.body:setFixedRotation(true)
-	
+
 	self.name = "enemy"
 
 	self.movDirection = love.math.random(2)
@@ -99,25 +99,25 @@ function Enemy:new(type, x, y) -- + class of enemy, warior, magician..
 	self.side = self.movDirection
 	self.step = love.math.random(10)
 	self.player_detect = false
-	
+
 	self.shape = love.physics.newRectangleShape(pcoords(self.width, self.height))
 	self.fixture = love.physics.newFixture(self.body, self.shape)
 	self.fixture:setRestitution(0.1)
 	self.fixture:setFriction(5)
 	self.canAttack = false
-	
+
 	self.body:setMass(self.type.mass)
-	
+
 	self.hp, self.maxHP = self.type.hp, self.type.hp
 
 	self.behaviour = self.type.behaviour
 	self.timer = 0
 	self.mana = 10
 	self.canDelete = false
-	
+
 	 -- also, there should be some agilities of different classes
 	 -- for ex. immortal, reduce fire dmg or smth like that
-	 
+
 	--[[
 	red = {}
 	self:randomGen(red)  --WTF IS THAT???????
@@ -141,12 +141,12 @@ function Enemy:randomGen(red)
 
 	if red[f] + red[e] + red[w] + red[a] < 3 and red[f] + red[e] + red[w] + red[a] > 8 then
 		self:randomGen(red)
-	end 
+	end
 end
 
 
 function Enemy:applyMagic(Dmg_fire, Dmg_water, Dmg_earth, Dmg_air)
-	-- check for special agil. of enemy , if no, then --> 
+	-- check for special agil. of enemy , if no, then -->
 	f = Dmg_fire  * self.fire_r
 	w = Dmg_water * self.water_r
 	e = Dmg_earth * self.earth_r
@@ -162,7 +162,7 @@ end
 
 function Enemy:attack()
 	local x, y = self:getMagicCoords()
-	if self.behaviour.attack == "magic" then 
+	if self.behaviour.attack == "magic" then
 		local typeMagic = math.random(self.behaviour.magic_type.q)
 		--love.event.quit(typeMagic)
 		typeMagic = self.behaviour.magic_type[typeMagic]
@@ -176,13 +176,13 @@ function Enemy:standartMovement()
 	if self.behaviour.movement_bd == "move" then
 
 		local xveloc, yveloc = self.body:getLinearVelocity()
-		
-		if (xveloc < plen(0.45)) and (self.movDirection == 1) then self.body:applyForce(100000, 0) 
-		elseif (xveloc > -plen(0.45)) and (self.movDirection == -1) then self.body:applyForce(-100000, 0) 
+
+		if (xveloc < plen(0.45)) and (self.movDirection == 1) then self.body:applyForce(100000, 0)
+		elseif (xveloc > -plen(0.45)) and (self.movDirection == -1) then self.body:applyForce(-100000, 0)
 		elseif (self.movDirection == 0) then
-			if (xveloc > 0.2) then 
+			if (xveloc > 0.2) then
 				self.body:applyForce(-10000, 0)
-			elseif (xveloc < -0.2) then 
+			elseif (xveloc < -0.2) then
 				self.body:applyForce(10000, 0)
 			end
 		end
@@ -190,13 +190,13 @@ function Enemy:standartMovement()
 	elseif self.behaviour.movement_bd == "slow_move" then
 
 		local xveloc, yveloc = self.body:getLinearVelocity()
-		
-		if (xveloc < plen(0.09)) and (self.movDirection == 1) then self.body:applyForce(100000, 0) 
-		elseif (xveloc > -plen(0.09)) and (self.movDirection == -1) then self.body:applyForce(-100000, 0) 
+
+		if (xveloc < plen(0.09)) and (self.movDirection == 1) then self.body:applyForce(100000, 0)
+		elseif (xveloc > -plen(0.09)) and (self.movDirection == -1) then self.body:applyForce(-100000, 0)
 		elseif (self.movDirection == 0) then
-			if (xveloc > 0.2) then 
+			if (xveloc > 0.2) then
 				self.body:applyForce(-10000, 0)
-			elseif (xveloc < -0.2) then 
+			elseif (xveloc < -0.2) then
 				self.body:applyForce(10000, 0)
 			end
 		end
@@ -210,54 +210,54 @@ function Enemy:trigerredMovement()
 	local xveloc, yveloc = self.body:getLinearVelocity()
 	local x1, y1 = self.body:getPosition()
 	local x2, y2 = player1.body:getPosition()
-	
+
 	if self.behaviour.movement_ad == "victim" then
-		if (x1 > x2) then 
+		if (x1 > x2) then
 			self.movDirection = 1
-			self.side = 1 * self.type.imgturn 
-		else 
-			self.movDirection = -1 
+			self.side = 1 * self.type.imgturn
+		else
+			self.movDirection = -1
 			self.side = -1 * self.type.imgturn
 		end
-		if (xveloc < plen(0.55)) and (self.movDirection == 1) then self.body:applyForce(100000, 0) 
+		if (xveloc < plen(0.55)) and (self.movDirection == 1) then self.body:applyForce(100000, 0)
 		elseif (xveloc > -plen(055)) and (self.movDirection == -1) then self.body:applyForce(-100000, 0)
 		end
 	elseif self.behaviour.movement_ad == "aggressive" then
-		if (x1 < x2) then 
+		if (x1 < x2) then
 			self.movDirection = 1
-			self.side = 1 * self.type.imgturn 
-		else 
-			self.movDirection = -1 
+			self.side = 1 * self.type.imgturn
+		else
+			self.movDirection = -1
 			self.side = -1 * self.type.imgturn
 		end
-		if ((flen(math.abs(x2 - x1)) < self.behaviour.playerdist + 0.02) and (flen(math.abs(x2 - x1)) > self.behaviour.playerdist - 0.02) ) then 
-			--if (xveloc < plen(0.1)) and (self.movDirection == 1) then self.body:applyForce(100000, 0) 
-			--elseif (xveloc > -plen(0.1)) and (self.movDirection == -1) then self.body:applyForce(-100000, 0) 
+		if ((flen(math.abs(x2 - x1)) < self.behaviour.playerdist + 0.02) and (flen(math.abs(x2 - x1)) > self.behaviour.playerdist - 0.02) ) then
+			--if (xveloc < plen(0.1)) and (self.movDirection == 1) then self.body:applyForce(100000, 0)
+			--elseif (xveloc > -plen(0.1)) and (self.movDirection == -1) then self.body:applyForce(-100000, 0)
 			--else
 			self.body:setLinearVelocity(0, yveloc)
 			self.canAttack = true
 			--end
 		elseif (flen(math.abs(x2 - x1)) > self.behaviour.playerdist) then
-			if (xveloc < plen(0.3)) and (self.movDirection == 1) then self.body:applyForce(100000, 0) 
+			if (xveloc < plen(0.3)) and (self.movDirection == 1) then self.body:applyForce(100000, 0)
 			elseif (xveloc > -plen(0.3)) and (self.movDirection == -1) then self.body:applyForce(-100000, 0) end
 			self.canAttack = false
 		elseif (flen(math.abs(x2 - x1)) < self.behaviour.playerdist) then
-			if (xveloc < plen(0.15)) and (self.movDirection == -1) then self.body:applyForce(100000, 0) 
+			if (xveloc < plen(0.15)) and (self.movDirection == -1) then self.body:applyForce(100000, 0)
 			elseif (xveloc > -plen(0.15)) and (self.movDirection == 1) then self.body:applyForce(-100000, 0) end
 			self.canAttack = true
-		end	
+		end
 	end
 	--check for the floor (in future)
 	--[[
 	-- find player, decide what to do
 	local xveloc, yveloc = self.body:getLinearVelocity()
-	
-	if (xveloc < plen(0.45)) and (self.movDirection == 1) then self.body:applyForce(100000, 0) 
-	elseif (xveloc > -plen(0.45)) and (self.movDirection == -1) then self.body:applyForce(-100000, 0) 
+
+	if (xveloc < plen(0.45)) and (self.movDirection == 1) then self.body:applyForce(100000, 0)
+	elseif (xveloc > -plen(0.45)) and (self.movDirection == -1) then self.body:applyForce(-100000, 0)
 	elseif (self.movDirection == 0) then
-		if (xveloc > 0.2) then 
+		if (xveloc > 0.2) then
 			self.body:applyForce(-100000, 0)
-		elseif (xveloc < -0.2) then 
+		elseif (xveloc < -0.2) then
 			self.body:applyForce(100000, 0)
 		end
 	end
@@ -275,7 +275,7 @@ function Enemy:detect()
 		for i, hit in ipairs(Ray.hitList) do
 			local obj = hit.fixture:getUserData()
 			if (obj.name == "player") then
-				if hit.fraction > 0.92 then canBeSeen = false end 
+				if hit.fraction > 0.92 then canBeSeen = false end
 				local player_fraction = hit.fraction
 				--player1.hp = hit.fraction
 				--break
@@ -291,11 +291,11 @@ function Enemy:detect()
 		--local y2 = y1 + plen(0.1)
 		--local y3 = y1 - plen(0.1)
 		--local xn, yn, fraction = self.fixture:rayCast(x1, y1, x2, y1, RayLength, 1)
-		
+
 		--[[
 		while (xn ~= nil) and (xn <= RayLength) do
 			hitx, hity = x1 + (x2 - x1) * fraction, y1 + (y1 - y1) * fraction
-			if player1.fixture:testPoint(hitx, hity) then 
+			if player1.fixture:testPoint(hitx, hity) then
 				return true
 			end
 			local x1 = hitx
@@ -340,9 +340,9 @@ end
 
 function Enemy:drawHP()
 	local len = self.hp/self.maxHP
-	
+
 	local x, y, x1, y1 = self.body:getWorldPoints(self.shape:getPoints())
-	
+
 	love.graphics.setColor(0, 0.5, 0)
 	love.graphics.rectangle("fill", x, y-plen(0.01), x1-x, plen(0.01))
 
@@ -354,19 +354,19 @@ end
 
 function Enemy:draw()
 	-- there should be more enemies sprites
-	-- self:choose_sprite(red) 
-	-- 		find max red[] and choose a sprite 
+	-- self:choose_sprite(red)
+	-- 		find max red[] and choose a sprite
 	--love.graphics.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
-	
+
 	local x, y = self.body:getWorldPoints(self.shape:getPoints())
-	if self.side == 1 then 
+	if self.side == 1 then
 		love.graphics.draw(self.image, x, y, 0, self.scale, self.scale)
 	elseif self.side == -1 then
 		love.graphics.draw(self.image, x+plen(self.width), y, 0, self.scale*self.side, self.scale)
 	end
-	
+
 	self:drawHP()
-end	
+end
 
 function Enemy:getCoords()
 	local x, y = self.body:getPosition()
