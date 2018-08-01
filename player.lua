1c3fcde32b4305120c020ca14460d850358c78de
Omek <lipskij2@gmail.com>
2018-08-01 23:08:01 +0300

    Category to npcs added

--- player.lua
+++ player.lua
@@ -37,6 +37,7 @@ function Player:new(mana, x, y)
 	self.fixture:setFriction(plen(0.001))
 	self.body:setMass(70) -- 70kg wizard
 	self.fixture:setCategory(2)
+	self.fixture.setMask(10)
 	self.fixture:setUserData(self)
 	return self
 end
